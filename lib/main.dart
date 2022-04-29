fffe0e7963ef95c8282bbdafe18df72312234bd0
tvhfrks12678 <tvhfrks12678@gmail.com>
2022-04-29 10:54:25 +0900

    feat: Step 6: Final touch

--- lib/main.dart
+++ lib/main.dart
@@ -73,7 +73,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: Column(
+        body: ListView(
           children: [
             Image.asset(
               'images/lake.jpg',
