25d8a6b9fc66450dde0d41706d6337739ef1b65b
tvhfrks12678 <tvhfrks12678@gmail.com>
2022-05-01 11:49:21 +0900

    learn: ListView

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,41 @@ class MyImageWidget extends StatelessWidget {
   const MyImageWidget({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return _buildGrid();
+    return _buildList();
+  }
+
+  Widget _buildList() {
+    return ListView(children: [
+      _tile('CineArts at the Empire', '85 W Portal Ave', Icons.theaters),
+      _tile('The Castro Theater', '429 Castro St', Icons.theaters),
+      _tile('Alamo Drafthouse Cinema', '2550 Mission St', Icons.theaters),
+      _tile('Roxie Theater', '3117 16th St', Icons.theaters),
+      _tile('United Artists Stonestown Twin', '501 Buckingham Way',
+          Icons.theaters),
+      _tile('AMC Metreon 16', '135 4th St #3000', Icons.theaters),
+      _tile('K\'s Kitchen', '757 Monterey Blvd', Icons.restaurant),
+      const Divider(),
+      _tile('Emmy\'s Restaurant', '1923 Ocean Ave', Icons.restaurant),
+      _tile('Chaiya Thai Restaurant', '272 Claremont Blvd', Icons.restaurant),
+      _tile('La Ciccia', '291 30th St', Icons.restaurant),
+    ]);
+  }
+
+  ListTile _tile(String title, String subtitle, IconData icon) {
+    return ListTile(
+      title: Text(
+        title,
+        style: const TextStyle(
+          fontWeight: FontWeight.w500,
+          fontSize: 20,
+        ),
+      ),
+      subtitle: Text(subtitle),
+      leading: Icon(
+        icon,
+        color: Colors.blue[500],
+      ),
+    );
   }
 
   Widget _buildGrid() => GridView.extent(
@@ -57,7 +91,12 @@ class MyImageWidget extends StatelessWidget {
   List<Container> _buildGridTileList(int count) => List.generate(
         count,
         (i) => Container(
-          child: Image.asset('images/lake.jpg'),
+          child: Column(
+            children: [
+              Text('title: $i'),
+              Image.asset('images/lake.jpg'),
+            ],
+          ),
         ),
       );
 
